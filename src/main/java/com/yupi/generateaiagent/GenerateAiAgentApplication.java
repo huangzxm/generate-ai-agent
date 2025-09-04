341d05f557e22192aaf10d6e61b052761ad28447
userHxm <1551421189@qq.com>
2025-09-04 23:26:37 +0800

    init

---
+++ src/main/java/com/yupi/generateaiagent/GenerateAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.generateaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateAiAgentApplication.class, args);
+    }
+
+}
