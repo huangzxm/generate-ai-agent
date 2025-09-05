da2dcedac73f5afdceb334b709bc364f6dbe1737
userHxm <1551421189@qq.com>
2025-09-06 01:36:36 +0800

    后端初始化

---
+++ src/main/java/com/yupi/generateaiagent/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.yupi.generateaiagent.controller;
+
+import com.yupi.generateaiagent.common.BaseResponse;
+import com.yupi.generateaiagent.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
