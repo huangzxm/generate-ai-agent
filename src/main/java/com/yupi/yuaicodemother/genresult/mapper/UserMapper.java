df391706dfafd51a937b398825dbae9fcda7abc0
userHxm <1551421189@qq.com>
2025-09-06 10:40:58 +0800

    SQL生成器初始化用户

---
+++ src/main/java/com/yupi/yuaicodemother/genresult/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.yupi.yuaicodemother.genresult.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.yupi.yuaicodemother.genresult.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/huangzxm">huangzxm</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/yupi/yuaicodemother/genresult/service/UserService.java
@@ -0,0 +1,13 @@
+package com.yupi.yuaicodemother.genresult.service;
+
+import com.mybatisflex.core.service.IService;
+import com.yupi.yuaicodemother.genresult.entity.User;
+
+/**
+ * 用户 服务层。
+ *
+ * @author <a href="https://github.com/huangzxm">huangzxm</a>
+ */
+public interface UserService extends IService<User> {
+
+}
