bf6840d6ccac15588d4face12dc38bf027185911
userHxm <1551421189@qq.com>
2025-09-06 23:55:55 +0800

    开发AI服务

---
+++ src/main/java/com/yupi/generateaiagent/ai/AiCodeGeneratorService.java
@@ -0,0 +1,26 @@
+package com.yupi.generateaiagent.ai;
+
+import com.yupi.generateaiagent.ai.model.HtmlCodeResult;
+import com.yupi.generateaiagent.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.SystemMessage;
+
+public interface AiCodeGeneratorService {
+
+    /**
+     * 生成 HTML 代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI 的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    HtmlCodeResult generateHtmlCode(String userMessage);
+
+    /**
+     * 生成多文件代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI 的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    MultiFileCodeResult generateMultiFileCode(String userMessage);
+}
