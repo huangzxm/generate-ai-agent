ed19dfaaaef274df0cca6a1f092152d38d862297
userHxm <1551421189@qq.com>
2025-09-08 23:47:43 +0800

    修改为AppId调用AI模式

--- src/test/java/com/yupi/generateaiagent/core/AiCodeGeneratorFacadeTest.java
+++ src/test/java/com/yupi/generateaiagent/core/AiCodeGeneratorFacadeTest.java
@@ -19,19 +19,19 @@ class AiCodeGeneratorFacadeTest {
 
     @Test
     void generateAndSaveCode() {
-        File file = aiCodeGeneratorFacade.generateAndSaveCode("生成一个登录页面，总共不超过 20 行代码", CodeGenTypeEnum.MULTI_FILE);
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站", CodeGenTypeEnum.MULTI_FILE, 1L);
         Assertions.assertNotNull(file);
     }
 
-//    @Test
-//    void generateAndSaveCodeStream() {
-//        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("生成一个登录页面，总共不超过 20 行代码", CodeGenTypeEnum.HTML);
-//        // 阻塞等待所有数据收集完成
-//        List<String> result = codeStream.collectList().block();
-//        // 验证结果
-//        Assertions.assertNotNull(result);
-//        // 拼接字符串，得到完整内容
-//        String completeContent = String.join("", result);
-//        Assertions.assertNotNull(completeContent);
-//    }
+    @Test
+    void generateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("任务记录网站", CodeGenTypeEnum.MULTI_FILE, 1L);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
+
 }
