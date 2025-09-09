569c0c3786a8e2581663387aeee369bd5dcaa584
userHxm <1551421189@qq.com>
2025-09-09 17:09:21 +0800

    重构代码

---
+++ src/test/java/com/yupi/yuaicodemother/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,28 @@
+package com.yupi.yuaicodemother.ai;
+
+
+import com.yupi.yuaicodemother.ai.model.HtmlCodeResult;
+import com.yupi.yuaicodemother.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个前端的博客，不超过 20 行");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult result = aiCodeGeneratorService.generateMultiFileCode("做个前端的留言板");
+        Assertions.assertNotNull(result);
+    }
+}
