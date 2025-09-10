4548e63510de8f0ca214f0851e6c9bc221b6217e
userHxm <1551421189@qq.com>
2025-09-10 16:33:20 +0800

    通过为每个appId创建一个service进行隔离

--- src/main/java/com/yupi/yuaicodemother/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/yupi/yuaicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -1,5 +1,7 @@
 package com.yupi.yuaicodemother.ai;
 
+import dev.langchain4j.community.store.memory.chat.redis.RedisChatMemoryStore;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.service.AiServices;
@@ -7,9 +9,6 @@ import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * AI 服务创建工厂
- */
 @Configuration
 public class AiCodeGeneratorServiceFactory {
 
@@ -19,16 +18,33 @@ public class AiCodeGeneratorServiceFactory {
     @Resource
     private StreamingChatModel streamingChatModel;
 
+    @Resource
+    private RedisChatMemoryStore redisChatMemoryStore;
+
     /**
-     * 创建 AI 代码生成器服务
-     *
-     * @return
+     * 根据 appId 获取服务
      */
-    @Bean
-    public AiCodeGeneratorService aiCodeGeneratorService() {
+    public AiCodeGeneratorService getAiCodeGeneratorService(long appId) {
+        // 根据 appId 构建独立的对话记忆
+        MessageWindowChatMemory chatMemory = MessageWindowChatMemory
+                .builder()
+                .id(appId)
+                .chatMemoryStore(redisChatMemoryStore)
+                .maxMessages(20)
+                .build();
         return AiServices.builder(AiCodeGeneratorService.class)
                 .chatModel(chatModel)
                 .streamingChatModel(streamingChatModel)
+                .chatMemory(chatMemory)
                 .build();
     }
+
+    /**
+     * 默认提供一个 Bean
+     */
+    @Bean
+    public AiCodeGeneratorService aiCodeGeneratorService() {
+        return getAiCodeGeneratorService(0L);
+    }
+
 }
