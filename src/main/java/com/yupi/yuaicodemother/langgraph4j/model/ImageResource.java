734f351a09262f5692715e97dc9c203c4edc4eed
userHxm <1551421189@qq.com>
2025-09-13 14:41:52 +0800

    内容图片收集工具

--- src/main/java/com/yupi/yuaicodemother/langgraph4j/model/ImageResource.java
+++ src/main/java/com/yupi/yuaicodemother/langgraph4j/model/ImageResource.java
@@ -1,6 +1,6 @@
 package com.yupi.yuaicodemother.langgraph4j.model;
 
-import com.yupi.yuaicodemother.langgraph4j.demo.enums.ImageCategoryEnum;
+import com.yupi.yuaicodemother.langgraph4j.model.enums.ImageCategoryEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
