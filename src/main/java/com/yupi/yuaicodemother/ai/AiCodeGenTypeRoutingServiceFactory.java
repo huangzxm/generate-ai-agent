e08b26cf72c5736722068b7ecba41cf34551170e
userHxm <1551421189@qq.com>
2025-09-15 09:19:41 +0800

    fix issue AI并发调用问题 解决Bean冲突

--- src/main/java/com/yupi/yuaicodemother/ai/AiCodeGenTypeRoutingServiceFactory.java
+++ src/main/java/com/yupi/yuaicodemother/ai/AiCodeGenTypeRoutingServiceFactory.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AiCodeGenTypeRoutingServiceFactory {
 
-    @Resource
+    @Resource(name = "openAiChatModel")
     private ChatModel chatModel;
 
     /**
